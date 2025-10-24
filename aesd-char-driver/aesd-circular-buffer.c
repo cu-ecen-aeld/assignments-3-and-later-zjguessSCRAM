c3a7977050c97cb04074592e8807c5e78d47807a
Zachary Guess <zachary.guess@scramsystems.com>
2025-10-24 11:34:52 -0400

    Completed PT.II, Electric Boogaloo

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -68,12 +68,18 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 * Any necessary locking must be handled by the caller
 * Any memory referenced in @param add_entry must be allocated by and/or must have a lifetime managed by the caller.
 */
-void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
+void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer,
+                                    const struct aesd_buffer_entry *add_entry)
 {
-    if (!buffer || !add_entry) return;
+    if (!buffer || !add_entry)
+        return;
 
     if (buffer->full) {
-        free((void*)buffer->entry[buffer->out_offs].buffptr);
+#ifdef __KERNEL__
+        kfree(buffer->entry[buffer->out_offs].buffptr);
+#else
+        free((void *)buffer->entry[buffer->out_offs].buffptr);
+#endif
         buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
     }
 
@@ -83,6 +89,7 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     if (buffer->in_offs == buffer->out_offs)
         buffer->full = true;
 }
+
 
 /**
 * Initializes the circular buffer described by @param buffer to an empty struct
